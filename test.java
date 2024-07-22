e269473a12cac0d314a2b992bc8b059f31429a8a
suchit chaudhari <suchitchaudhari17@gmail.com>
2024-07-22 10:57:14 +0530

    first commit

---
+++ test.java
@@ -0,0 +1,24 @@
+
+public class test {
+	 static int a;
+	 static float b;
+	 static char c;
+	 static boolean d;
+	 static double e;
+	 static byte f;
+	 static short s;
+	 static long l;
+     static String h;
+public static void main(String[] args) {
+	
+	System.out.println("int = "+a);
+	System.out.println("float = "+b);
+	System.out.println("char = "+c);
+	System.out.println("boolean = "+d);
+	System.out.println("byte = "+f);
+	System.out.println("short = "+s);
+	System.out.println("long = "+l);
+	System.out.println("double = "+e);
+	System.out.println("String =" +h);
+}
+}
